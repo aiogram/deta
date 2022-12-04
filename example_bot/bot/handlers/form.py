0d5f8af54c1c85e0bf2b42bb51eb455851d578a1
JRoot Junior <jroot.junior@gmail.com>
2022-12-05 00:35:09 +0200

    Prettify commands

--- example_bot/bot/handlers/form.py
+++ example_bot/bot/handlers/form.py
@@ -16,7 +16,7 @@ class Form(StatesGroup):
     language = State()
 
 
-@form_router.message(Command(commands=["start"]))
+@form_router.message(Command("start"))
 async def command_start(message: Message, state: FSMContext) -> None:
     await state.set_state(Form.name)
     await message.answer(
@@ -25,7 +25,7 @@ async def command_start(message: Message, state: FSMContext) -> None:
     )
 
 
-@form_router.message(Command(commands=["cancel"]))
+@form_router.message(Command("cancel"))
 @form_router.message(F.text.casefold() == "cancel")
 async def cancel_handler(message: Message, state: FSMContext) -> None:
     """
